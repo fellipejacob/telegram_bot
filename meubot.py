0597328e03d80bd53ab7fc31a93ecf46fd22576b
fellipejacob <590792@gmail.com>
2023-05-15 11:53:37 -0400

    feat: read, update and delete func added

--- meubot.py
+++ meubot.py
@@ -1,17 +1,21 @@
 import logging
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, ConversationHandler, MessageHandler, filters
-from sqlalchemy import create_engine, Column, Integer, String, Float
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-
 # Enable logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-GENDER, PHOTO, DOCUMENT_NUMBER, NAME = range(4)
+NAME, DOCUMENT_NUMBER, DOCUMENT_NUMBER_EXCLUDE, DOCUMENT_NUMBER_UPDATE, NAME_UPDATE = range(5)
 
 Base = declarative_base()
 
@@ -20,17 +24,14 @@ class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     chat_id = Column(Integer)
-    gender = Column(String)
-    photo = Column(String)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    bio = Column(String)
+    name = Column(String)
+    document_number = Column(String)
 
     def __repr__(self):
-        return f"<User(chat_id='{self.chat_id}', gender='{self.gender}', photo='{self.photo}', latitude='{self.latitude}', longitude='{self.longitude}', bio='{self.bio}')>"
+        return f"<User(chat_id='{self.chat_id}', name='{self.name}', DocumentNumber ='{self.document_number}')>"
 
 
-engine = create_engine('sqlite:///user_data.db')
+engine = create_engine('sqlite:///userData.db')
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 
@@ -38,106 +39,64 @@ user = User()
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Starts the conversation and asks the user about their gender."""
-    reply_keyboard = [["Man", "Woman", "Other"]]
-
+    """Starts the conversation and asks for the user's document number."""
     await update.message.reply_text(
-        "Hi! My name is Professor Bot. I will hold a conversation with you. "
-        "Send /cancel to stop talking to me.\n\n"
-        "Are you a boy or a girl?",
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Boy or Girl?"
-        ),
-    )
-
-    return GENDER
-
-
-async def gender(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the selected gender and asks for a photo."""
-    user = update.message.from_user
-    logger.info("Gender of %s: %s", user.first_name, update.message.text)
-    await update.message.reply_text(
-        "I see! Please send me a photo for the registry "
-        "or send /skip if you don't want to.",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-    return PHOTO
-
-
-async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the photo and asks for a document number."""
-    user = update.message.from_user
-    await update.message.reply_text(
-        "Great! Now, please send me your document number, or send /skip if you don't want to."
-    )
-
-    return DOCUMENT_NUMBER
-
-
-async def skip_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Skips the photo and asks for a document number."""
-    user = update.message.from_user
-    logger.info("User %s did not send a photo.", user.first_name)
-    await update.message.reply_text(
-        "I bet you look great! Now, send me your document number please, or send /skip."
+        "Olá, bem-vindo ao sistema OzaBot. Eu vou fazer seu cadastro. "
+        "Digite /cancel a qualquer momento para sair.\n\n"
+        "Por favor, me diga o número do seu documento sem pontuação:"
     )
 
     return DOCUMENT_NUMBER
 
 
 async def document_number(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the document number and asks for some info about the user."""
+    """Stores the user's document number and asks for their name."""
     user = update.message.from_user
     document_number = update.message.text
-    logger.info(
-        "Document number of %s: %s", user.first_name, document_number
-    )
-    await update.message.reply_text(
-        "Thanks! Finally, tell me your full name."
-    )
 
-    return NAME
+    # Check if the user is already registered in the database
+    session = Session()
+    existing_user = session.query(User).filter(User.document_number == document_number).first()
+    session.close()
 
+    if existing_user:
+        await update.message.reply_text("Você já está registrado no nosso sistema.")
+        return ConversationHandler.END
 
-async def skip_document(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Skips the document and asks for info about the user."""
-    user = update.message.from_user
-    logger.info("User %s did not send a document number.", user.first_name)
-    await update.message.reply_text(
-        "You seem a bit paranoid! At last, tell me something about yourself."
-    )
+    # Store the document number in user_data
+    context.user_data['document_number'] = document_number
+
+    await update.message.reply_text("Por favor, me diga seu nome completo:")
 
     return NAME
 
 
 async def name(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the info about the user and ends the conversation."""
+    """Stores the user's name and checks if they are already registered."""
     user = update.message.from_user
-    logger.info("Bio of %s: %s", user.first_name, update.message.text)
+    name = update.message.text
 
-    # Store the bio in the database
-    session = context.session
+    # Retrieve the document number from user_data
+    document_number = context.user_data.get('document_number')
+
+    # Store the user's name and document number in the database
+    session = Session()
     chat_id = user.id
-    bio_text = update.message.text
-    user = session.query(User).filter_by(chat_id=chat_id).first()
-    user.bio = bio_text
+    user = User(chat_id=chat_id, name=name, document_number=document_number)
+    session.add(user)
     session.commit()
+    session.close()
 
-    await update.message.reply_text("Thank you! I hope we can talk again some day.")
+    await update.message.reply_text("Obrigado por usar nosso sistema!")
 
     return ConversationHandler.END
-
 
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Cancels and ends the conversation."""
     user = update.message.from_user
     logger.info("User %s canceled the conversation.", user.first_name)
-    await update.message.reply_text(
-        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
-    )
+    await update.message.reply_text("Espero que esteja tudo bem. Até uma proxima vez!")
 
     # Close the database session
     session = context["session"]
@@ -146,21 +105,101 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     return ConversationHandler.END
 
 
+async def exclude(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Handles the 'Exclude' command."""
+    await update.message.reply_text("Por favor, digite o número do documento do usuário que deseja excluir:")
+
+    return DOCUMENT_NUMBER_EXCLUDE
+
+
+async def document_number_exclude(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Checks if the user exists in the database and deletes them."""
+    document_number = update.message.text
+
+    # Check if the user is registered in the database
+    session = Session()
+    existing_user = session.query(User).filter(User.document_number == document_number).first()
+
+    if existing_user:
+        # User exists, delete them from the database
+        session.delete(existing_user)
+        session.commit()
+        await update.message.reply_text("Usuário excluído com sucesso.")
+    else:
+        # User not found in the database
+        await update.message.reply_text("Usuário não encontrado.")
+
+    session.close()
+
+    return ConversationHandler.END
+
+
+async def update_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Handles the 'Update' command."""
+    await update.message.reply_text("Por favor, digite o número do documento do usuário que deseja atualizar:")
+
+    return DOCUMENT_NUMBER_UPDATE
+
+
+async def document_number_update(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Checks if the user exists in the database and updates their name."""
+    document_number = update.message.text
+
+    # Check if the user is registered in the database
+    session = Session()
+    existing_user = session.query(User).filter(User.document_number == document_number).first()
+
+    if existing_user:
+        # User exists, prompt for the updated name
+        await update.message.reply_text("Usuário encontrado. Por favor, digite o novo nome do usuário:")
+        context.user_data['document_number'] = document_number
+        return NAME_UPDATE
+    else:
+        # User not found in the database
+        await update.message.reply_text("Usuário não encontrado.")
+
+    session.close()
+
+    return ConversationHandler.END
+
+
+async def name_update(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Updates the user's name."""
+    user = update.message.from_user
+    name = update.message.text
+
+    # Retrieve the document number from user_data
+    document_number = context.user_data.get('document_number')
+
+    # Update the user's name in the database
+    session = Session()
+    existing_user = session.query(User).filter(User.document_number == document_number).first()
+    existing_user.name = name
+    session.commit()
+    session.close()
+
+    await update.message.reply_text("Nome do usuário atualizado com sucesso.")
+
+    return ConversationHandler.END
+
+
 def main() -> None:
     """Run the bot."""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token("token_here").build()
+    application = Application.builder().token("6118529672:AAHZBakVYjD0P28HHh9IaZ4qPBdds9LdHUs").build()
 
-    # Add conversation handler with the states GENDER, PHOTO, DOCUMENT and FULL NAME
+    # Add conversation handler with the states FULL NAME and DOCUMENT NUMBER
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", start)],
+        entry_points=[
+            CommandHandler("start", start),
+            CommandHandler("update", update_user),
+            CommandHandler("exclude", exclude)
+        ],
         states={
-            GENDER: [MessageHandler(filters.Regex("^(Man|Woman|Other)$"), gender)],
-            PHOTO: [MessageHandler(filters.PHOTO, photo),
-                    CommandHandler("skip", skip_photo)],
-            DOCUMENT_NUMBER: [
-                MessageHandler(filters.TEXT & ~filters.COMMAND, document_number),
-                CommandHandler("skip", skip_document)],
+            DOCUMENT_NUMBER: [MessageHandler(filters.TEXT & ~filters.COMMAND, document_number)],
+            DOCUMENT_NUMBER_EXCLUDE: [MessageHandler(filters.TEXT & ~filters.COMMAND, document_number_exclude)],
+            DOCUMENT_NUMBER_UPDATE: [MessageHandler(filters.TEXT & ~filters.COMMAND, document_number_update)],
+            NAME_UPDATE: [MessageHandler(filters.TEXT & ~filters.COMMAND, name_update)],
             NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, name)],
         },
         fallbacks=[CommandHandler("cancel", cancel)],
@@ -173,4 +212,10 @@ def main() -> None:
 
 
 if __name__ == "__main__":
+    # Setup logging and other configurations if needed
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO
+    )
+    # Call the main function
     main()
